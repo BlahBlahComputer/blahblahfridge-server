1e7a3968fd2d1afe8d003555dbf484f9f812f0c4
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-03 19:37:13 +0900

    initial commit

---
+++ src/main/java/sogang/capstone/blahblahfridge/BlahblahfridgeApplication.java
@@ -0,0 +1,13 @@
+package sogang.capstone.blahblahfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlahblahfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlahblahfridgeApplication.class, args);
+	}
+
+}
