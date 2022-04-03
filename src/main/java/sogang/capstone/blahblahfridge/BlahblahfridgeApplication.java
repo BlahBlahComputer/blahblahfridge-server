da6867351c98b4ac59edab625d671cedfaadd864
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-03 19:51:18 +0900

    setup project

--- src/main/java/sogang/capstone/blahblahfridge/BlahblahfridgeApplication.java
+++ src/main/java/sogang/capstone/blahblahfridge/BlahblahfridgeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlahblahfridgeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlahblahfridgeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlahblahfridgeApplication.class, args);
+    }
 
 }
