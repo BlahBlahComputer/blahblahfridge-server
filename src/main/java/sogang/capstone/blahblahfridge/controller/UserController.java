b1a516a9a5495ec1344e4c62ceea3820599b69c7
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-05-28 03:54:42 +0900

    feat: user edit

--- src/main/java/sogang/capstone/blahblahfridge/controller/UserController.java
+++ src/main/java/sogang/capstone/blahblahfridge/controller/UserController.java
@@ -1,14 +1,19 @@
 package sogang.capstone.blahblahfridge.controller;
 
 import java.util.Optional;
+import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import sogang.capstone.blahblahfridge.config.CommonResponse;
+import sogang.capstone.blahblahfridge.controller.request.UserImageRequest;
+import sogang.capstone.blahblahfridge.controller.request.UserNameRequest;
 import sogang.capstone.blahblahfridge.domain.User;
 import sogang.capstone.blahblahfridge.dto.UserDTO;
 import sogang.capstone.blahblahfridge.exception.NotFoundException;
@@ -35,4 +40,37 @@ public class UserController {
         return CommonResponse.onSuccess(userDTO);
     }
 
+    @PutMapping(value = "/{id}/image", produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public CommonResponse<UserDTO> editUserImageById(@PathVariable("id") Long id,
+        @Valid @RequestBody UserImageRequest userImageRequest) {
+        Optional<User> user = repo.findById(id);
+        if (user.isEmpty()) {
+            throw new NotFoundException("해당 유저가 없습니다.");
+        }
+        user.ifPresent(origin -> {
+            origin.setImage(userImageRequest.getImage());
+            repo.save(origin);
+        });
+
+        UserDTO userDTO = new UserDTO(user.get());
+        return CommonResponse.onSuccess(userDTO);
+    }
+
+    @PutMapping(value = "/{id}/name", produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public CommonResponse<UserDTO> editUserNameById(@PathVariable("id") Long id,
+        @Valid @RequestBody UserNameRequest userNameRequest) {
+        Optional<User> user = repo.findById(id);
+        if (user.isEmpty()) {
+            throw new NotFoundException("해당 유저가 없습니다.");
+        }
+        user.ifPresent(origin -> {
+            origin.setUsername(userNameRequest.getUsername());
+            repo.save(origin);
+        });
+
+        UserDTO userDTO = new UserDTO(user.get());
+        return CommonResponse.onSuccess(userDTO);
+    }
 }
--- src/test/java/sogang/capstone/blahblahfridge/unit/controller/UserControllerTest.java
+++ src/test/java/sogang/capstone/blahblahfridge/unit/controller/UserControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import sogang.capstone.blahblahfridge.config.CommonResponse;
 import sogang.capstone.blahblahfridge.controller.UserController;
+import sogang.capstone.blahblahfridge.controller.request.UserImageRequest;
+import sogang.capstone.blahblahfridge.controller.request.UserNameRequest;
 import sogang.capstone.blahblahfridge.domain.User;
 import sogang.capstone.blahblahfridge.dto.UserDTO;
 import sogang.capstone.blahblahfridge.exception.NotFoundException;
@@ -58,4 +60,101 @@ public class UserControllerTest {
         // then
         Assertions.assertEquals(CommonResponse.onSuccess(new UserDTO(user)), result);
     }
+
+    @Test
+    @DisplayName("유저 이미지 수정 시 유저 없을 경우, 예외 처리 되는지 확인")
+    public void testIfUserImageEditFailWhenUserNotExistThenThrowException() {
+        // given
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        Mockito.when(mockUserRepository.findById(1L))
+            .thenThrow(new NotFoundException("해당 유저가 없습니다."));
+        UserImageRequest userImageRequest = new UserImageRequest("image");
+
+        // when
+        UserController userController = new UserController(
+            mockUserRepository
+        );
+
+        // then
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            userController.editUserImageById(1L, userImageRequest);
+        });
+    }
+
+    @Test
+    @DisplayName("유저 이미지 수정 성공 시, 수정 결과 반환 되는지 확인")
+    public void testIfUserImageEditSuccessThenReturnEditedUser() {
+        // given
+        User user = User.builder()
+            .id(1L)
+            .username("user")
+            .image("image")
+            .authenticationCode("asdfasdf")
+            .provider("kakao")
+            .build();
+
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        Mockito.when(mockUserRepository.findById(1L))
+            .thenReturn(Optional.ofNullable(user));
+        UserImageRequest userImageRequest = new UserImageRequest("new image");
+        user.setImage(userImageRequest.getImage());
+
+        // when
+        UserController userController = new UserController(
+            mockUserRepository
+        );
+        CommonResponse<UserDTO> result = userController.editUserImageById(1L, userImageRequest);
+
+        // then
+        Assertions.assertEquals(CommonResponse.onSuccess(new UserDTO(user)), result);
+    }
+
+    @Test
+    @DisplayName("유저 이름 수정 시 유저 없을 경우, 예외 처리 되는지 확인")
+    public void testIfUserNameEditFailWhenUserNotExistThenThrowException() {
+        // given
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        Mockito.when(mockUserRepository.findById(1L))
+            .thenThrow(new NotFoundException("해당 유저가 없습니다."));
+        UserNameRequest userNameRequest = new UserNameRequest("name");
+
+        // when
+        UserController userController = new UserController(
+            mockUserRepository
+        );
+
+        // then
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            userController.editUserNameById(1L, userNameRequest);
+        });
+    }
+
+    @Test
+    @DisplayName("유저 이름 수정 성공 시, 수정 결과 반환 되는지 확인")
+    public void testIfUserNameEditSuccessThenReturnEditedUser() {
+        // given
+        User user = User.builder()
+            .id(1L)
+            .username("user")
+            .image("image")
+            .authenticationCode("asdfasdf")
+            .provider("kakao")
+            .build();
+
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        Mockito.when(mockUserRepository.findById(1L))
+            .thenReturn(Optional.ofNullable(user));
+        UserNameRequest userNameRequest = new UserNameRequest("new image");
+        user.setUsername(userNameRequest.getUsername());
+
+        // when
+        UserController userController = new UserController(
+            mockUserRepository
+        );
+        CommonResponse<UserDTO> result = userController.editUserNameById(1L, userNameRequest);
+
+        // then
+        Assertions.assertEquals(CommonResponse.onSuccess(new UserDTO(user)), result);
+    }
+
 }
