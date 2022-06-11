b49738fe6a41503021218a4dba894eaaf176369c
Dongmin Seo <sdm0325@gmail.com>
2022-06-11 16:31:43 +0900

    fix:  문법 수정

--- src/main/java/sogang/capstone/blahblahfridge/dto/AnalyzeRequestDTO.java
+++ src/main/java/sogang/capstone/blahblahfridge/dto/AnalyzeRequestDTO.java
@@ -1,14 +1,16 @@
 package sogang.capstone.blahblahfridge.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @Builder
-@Getter
 public class AnalyzeRequestDTO {
     private String bucket;
 
